6ffcb10a8210336d255309f2997473533a715d51
Mikhail Baranov <baranov@arrival.com>
2019-07-13 11:06:32 +0300

    Bump version

--- custom_components/yandex_maps/sensor.py
+++ custom_components/yandex_maps/sensor.py
@@ -12,7 +12,7 @@ import homeassistant.helpers.config_validation as cv
 from homeassistant.components.sensor import PLATFORM_SCHEMA
 from homeassistant.helpers.entity import Entity
 
-__version__ = '0.0.4'
+__version__ = '0.0.5'
 
 CONF_NAME = 'name'
 CONF_START = 'start'
